f8d89d96a07430619e57d3da93a47b4a77a13c3e
Dave Shah <david.a.shah@gmail.com>
2021-05-22 18:49:35 -0400

    Setup page

--- lib/bijou_web/controllers/page_controller.ex
+++ lib/bijou_web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule BijouWeb.PageController do
   use BijouWeb, :controller
+  alias Bijou.Urls
 
   def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  @spec create(Plug.Conn.t(), nil | maybe_improper_list | map) :: Plug.Conn.t()
+  def create(conn, params) do
+    %{short_url: s, errors: e} = Urls.create_short_url(params["url"])
+
     render(conn, "index.html")
   end
 end
--- test/bijou_web/controllers/page_controller_test.exs
+++ test/bijou_web/controllers/page_controller_test.exs
@@ -3,6 +3,20 @@ defmodule BijouWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Bijou!"
+  end
+
+  describe "POST /" do
+    test "posting with a valid url", %{conn: conn} do
+      before_short_url_count = Enum.count(Bijou.Repo.all(Bijou.ShortUrl))
+
+      conn = post(conn, "/", url: "https://www.google.com")
+      assert html_response(conn, 200)
+
+      after_short_url_count = Enum.count(Bijou.Repo.all(Bijou.ShortUrl))
+
+      assert after_short_url_count == before_short_url_count + 1
+      # TODO: Could test by querying for www.google.com
+    end
   end
 end
